c726720c867448d97cce53ec721386a37d47892d
montien1992 <montien.ng@gmail.com>
2026-05-24 20:50:03 +0700

    refactor: centralize database connection logic in migrations to use api/db.php

--- migrate_db.php
+++ migrate_db.php
@@ -1,20 +1,7 @@
 <?php
-$host = '127.0.0.1';
-$db   = 'pnpman_db';
-$user = 'root';
-$pass = '';
-$charset = 'utf8mb4';
-
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$options = [
-    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES   => false,
-    PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
-];
+require_once __DIR__ . '/api/db.php';
 
 try {
-    $pdo = new PDO($dsn, $user, $pass, $options);
     
     // 1. Change role to VARCHAR
     $pdo->exec("ALTER TABLE assignments MODIFY role VARCHAR(50) NOT NULL");
